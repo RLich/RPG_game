ac274acc81c1e23dcc3e24759bd1adea6099823f
Rafał Lichocki <rafal.lichocki@gmail.com>
2024-02-29 23:02:01 +0100

    Moved default json values to a new file

---
+++ json_blueprints.py
@@ -0,0 +1,203 @@
+items_list = [
+    {
+        "id": 0,
+        "name": "Gold",
+        "value": 1,
+        "quantity": 10
+    },
+    {
+        "id": 1,
+        "name": "Health potion",
+        "value": 10,
+        "restore": 10,
+        "quantity": 3
+    },
+{
+        "id": 2,
+        "name": "Mana potion",
+        "value": 10,
+        "restore": 10,
+        "quantity": 3
+    }
+]
+
+spells_list = [
+    {
+        "id": 1,
+        "name": "Firebolt",
+        "power": 6,
+        "mana_cost": 2,
+        "value": 10,
+        "quantity": 1,
+        "combat": True,
+        "camp": False,
+        "healing": False
+    },
+    {
+        "id": 2,
+        "name": "Fireball",
+        "power": 12,
+        "mana_cost": 4,
+        "value": 20,
+        "quantity": 0,
+        "combat": True,
+        "camp": False,
+        "healing": False
+    },
+    {
+        "id": 3,
+        "name": "Elemental Missiles",
+        "power": "6d6",
+        "mana_cost": 5,
+        "value": 30,
+        "quantity": 0,
+        "combat": True,
+        "camp": False,
+        "healing": False
+    },
+{
+        "id": 4,
+        "name": "Healing Light",
+        "power": 5,
+        "mana_cost": 2,
+        "value": 20,
+        "quantity": 0,
+        "combat": True,
+        "camp": True,
+        "healing": True
+    }
+]
+
+weapons_list = [
+    {
+        "id": 0,
+        "name": "Trusty knife",
+        "damage": 3,
+        "value": 5
+    }
+]
+
+characters_list = [
+    {
+        "id": 0,
+        "encounter": 1,
+        "name": "Hero",
+        "hp": 20,
+        "max_hp": 20,
+        "str": 5,
+        "int": 5,
+        "mp": 10,
+        "speed": 5,
+        "xp": 0,
+        "level": 1,
+        "max_mp": 10
+    },
+    {
+        "id": 1,
+        "name": "Goblin",
+        "hp": 5,
+        "str": 5,
+        "int": 3,
+        "mp": 3,
+        "speed": 7,
+        "weapon_dmg": 3,
+        "xp": 5,
+        "max_hp": 5
+    },
+    {
+        "id": 2,
+        "name": "Zombie",
+        "hp": 12,
+        "str": 10,
+        "int": 1,
+        "mp": 1,
+        "speed": 2,
+        "weapon_dmg": 0,
+        "xp": 5,
+        "max_hp": 12
+    },
+    {
+        "id": 3,
+        "name": "Bandit",
+        "hp": 10,
+        "str": 7,
+        "int": 5,
+        "mp": 5,
+        "speed": 5,
+        "weapon_dmg": 3,
+        "xp": 5,
+        "max_hp": 10
+    },
+    {
+        "id": 4,
+        "name": "Marauder",
+        "hp": 15,
+        "str": 10,
+        "int": 8,
+        "mp": 8,
+        "speed": 5,
+        "weapon_dmg": 5,
+        "xp": 10,
+        "max_hp": 15
+    },
+    {
+        "id": 5,
+        "name": "Young Orc",
+        "hp": 18,
+        "str": 10,
+        "int": 10,
+        "mp": 10,
+        "speed": 6,
+        "weapon_dmg": 5,
+        "xp": 10,
+        "max_hp": 10
+    },
+    {
+        "id": 6,
+        "name": "Skeleton Warrior",
+        "hp": 12,
+        "str": 8,
+        "int": 5,
+        "mp": 5,
+        "speed": 4,
+        "weapon_dmg": 8,
+        "xp": 10,
+        "max_hp": 12
+    },
+    {
+        "id": 7,
+        "name": "Hobgoblin",
+        "hp": 10,
+        "str": 7,
+        "int": 5,
+        "mp": 5,
+        "speed": 6,
+        "weapon_dmg": 10,
+        "xp": 15,
+        "max_hp": 10
+    },
+    {
+        "id": 8,
+        "name": "Orc",
+        "hp": 22,
+        "str": 12,
+        "int": 5,
+        "mp": 5,
+        "speed": 5,
+        "weapon_dmg": 7,
+        "xp": 15,
+        "max_hp": 22
+    },
+    {
+        "id": 9,
+        "name": "Meat Golem",
+        "hp": 30,
+        "str": 20,
+        "int": 5,
+        "mp": 5,
+        "speed": 1,
+        "weapon_dmg": 0,
+        "xp": 15,
+        "max_hp": 30
+    }
+]
